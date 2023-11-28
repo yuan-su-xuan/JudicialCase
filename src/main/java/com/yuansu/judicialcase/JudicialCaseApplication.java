8a51990f9284e5200d68d07217f64ac3c8fab4d6
zjx <1228358775@qq.com>
2023-11-28 12:08:45 +0800

    feature: default

---
+++ src/main/java/com/yuansu/judicialcase/JudicialCaseApplication.java
@@ -0,0 +1,13 @@
+package com.yuansu.judicialcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudicialCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JudicialCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuansu/judicialcase/JudicialCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuansu.judicialcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudicialCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
